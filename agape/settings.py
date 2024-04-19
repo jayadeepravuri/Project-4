612760cafccf7a23b40ac737570a069b35aaf05c
jayadeepravuri <126668617+jayadeepravuri@users.noreply.github.com>
2024-04-19 09:14:12 +0200

    Update settings.py

--- agape/settings.py
+++ agape/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["8000-jayadeepravuri-agape-7nrdjkhoge8.ws-eu110.gitpod.io",
                  'localhost', '127.0.0.1',
                  '.herokuapp.com']
